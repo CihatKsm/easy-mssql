0698cfdeef7f68194485031605a3fee4ec36d979
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-12-10 23:32:25 +0300

    Add SetConfig to database module and update simple_server.js to fetch user email

--- simple_server.js
+++ simple_server.js
@@ -1,15 +1,16 @@
 const config = require('./config');
 
-const { Connect, Table, Request } = require('./database');
+const { Connect, Table, Request, SetConfig } = require('./database');
 const date = () => new Date()
 
 console.log(date(), 'System opened!')
 
+//SetConfig.logingMode(true);
 Connect(config.sql)
 
 setTimeout(async () => {
-    // const user = await Table('company').findOne({ id: 1 });
-    // console.log(user);
+    const user = await Table('account').findOne({ id: 250320091115 });
+    console.log(user.email);
 
     // const info = await Table('company').functions.info();
     // console.log(info); 
