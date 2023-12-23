89393707462e8ddc08300d3ce3bbcfcda1c79c2b
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-12-23 23:33:27 +0300

    Add optional run function to connectSql.js

--- db/connectSql.js
+++ db/connectSql.js
@@ -4,9 +4,10 @@ const config = require('./config');
 /**
  * 
  * @param {*} sqlConfig This value is the sql configuration to be connected to the database.
+ * @param {*} run This value is the function to be executed after the connection is established.
  * @returns 
  */
-module.exports = async (sqlConfig) => {
+module.exports = async (sqlConfig, run) => {
     if (!sqlConfig || typeof sqlConfig !== 'object') {
         if (config.get.logingMode())
             console.log({ status: false, message: 'The sqlConfig parameter is not an object.' });
@@ -28,13 +29,14 @@ module.exports = async (sqlConfig) => {
     return await new Promise(async (resolve) => {
         try {
             sql.connect(sqlConfig, (err) => {
+                if (run && typeof run === 'function') return run(sqlConfig, err);
                 if (err) resolve(false);
                 else resolve(true);
-
                 if (config.get.logingMode())
                     console.log({ status: err ? false : true, message: err ? String(err).split('\n')[0] : 'Success' });
             });
         } catch (err) {
+            if (run && typeof run === 'function') return run(sqlConfig, err);
             resolve(false);
             if (config.get.logingMode())
                 console.log({ status: false, message: String(err).split('\n')[0] });
