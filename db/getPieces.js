49396dce9b4b8ac74aa7051175945b7cc3c27b74
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2024-01-23 21:58:48 +0300

    Fix value formatting in getPieces.js

--- db/getPieces.js
+++ db/getPieces.js
@@ -11,5 +11,10 @@ module.exports = (data) => {
     if (!data || typeof data !== 'object' || !dataString?.startsWith('{') || !dataString?.endsWith('}')) return datas;
     datas.keys = Object?.keys(data) || [];
     datas.values = Object?.values(data)?.map(v => Fixing.value(`'${v}'`)) || [];
+    datas.values = datas.values.map(m => {
+        let value = m.slice(1, -1);
+        value = value.replaceAll(`\'`, '’');
+        return `'${value}'`;
+    })
     return datas;
 }
